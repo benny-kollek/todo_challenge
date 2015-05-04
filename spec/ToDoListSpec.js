9f1254d2022fd41305cb787c256de6ea82c9ada2
Benjamin Kollek <benkollek@gmail.com>
2015-05-04 12:38:50 +0100

    Testing: front end

--- spec/ToDoListSpec.js
+++ spec/ToDoListSpec.js
@@ -1,17 +1,29 @@
-describe("Player", function() {
+describe("ToDoList", function() {
 
-  beforeEach(function() {
-    taskList = new ToDoList;
+  describe("Back End", function (){
+    beforeEach(function() {
+      taskList = new ToDoList;
+    });
+
+    it("should start with no tasks", function() {
+      expect(taskList.tasks.length).toEqual(0);
+    });
+
+    it("can add a task", function() {
+      taskList.addTask("Walk the dog");
+      taskList.addTask("Work out");
+      expect(taskList.tasks[0]).toEqual("Walk the dog");
+      expect(taskList.tasks[1]).toEqual("Work out");
+    });
   });
 
-  it("should start with no tasks", function() {
-    expect(taskList.tasks.length).toEqual(0);
-  });
+  describe("Front End", function() {
+    beforeEach(function() {
+      loadFixtures('../index.html')
+    });
 
-  it("can add a task", function() {
-    taskList.addTask("Walk the dog");
-    taskList.addTask("Work out");
-    expect(taskList.tasks[0]).toEqual("Walk the dog");
-    expect(taskList.tasks[1]).toEqual("Work out");
+    it("has input field", function() {
+      expect($('#input-field')).toBeInDom();
+    });
   });
 });
