6f18a09e5568649bc9c9da92fbaf523fed8df2cf
Benjamin Kollek <benkollek@gmail.com>
2015-05-04 12:33:52 +0100

    Can add a task

--- spec/ToDoListSpec.js
+++ spec/ToDoListSpec.js
@@ -1,7 +1,17 @@
 describe("Player", function() {
 
-  it("should start with no tasks", function() {
+  beforeEach(function() {
     taskList = new ToDoList;
+  });
+
+  it("should start with no tasks", function() {
     expect(taskList.tasks.length).toEqual(0);
+  });
+
+  it("can add a task", function() {
+    taskList.addTask("Walk the dog");
+    taskList.addTask("Work out");
+    expect(taskList.tasks[0]).toEqual("Walk the dog");
+    expect(taskList.tasks[1]).toEqual("Work out");
   });
 });
--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,6 +1,6 @@
 function ToDoList() {
   this.tasks = [];
 }
-Player.prototype.addTask = function(text) {
-  this.tasks.append = text;
+ToDoList.prototype.addTask = function(text) {
+  this.tasks.push(text);
 };
